6b6d25fdf5ae3a1c5ff76886f64e0d1935fefd98
ShihaburR <shihaburr1@gmail.com>
2020-03-14 00:00:06 +0000

    unable to make a selector to change article each time but displays all json information

--- article.js
+++ article.js
@@ -1,21 +1,126 @@
-let request = new XMLHttpRequest();
+//let request = new XMLHttpRequest();
 
-function GET() {
+function selector(){
+    get1()
+}
+function get1() {
   fetch("./data/article-1.json")
-    .then(data => {
-        return display();
+    .then(function (response){
+          return response.json();
     })
-    .then(response =>{
-        console.log(response);
+    .then(function(data){
+        console.log(data);
+        return display(data);
     })
     .catch(function (err){
         console.log(err);
     });
 }
 
-function display(){
-    const main = document.getElementById("myData");
-    const obj = JSON.parse('./data/article-1.json');
-    document.getElementById("title").innerHTML = obj.title;
+function get2() {
+    fetch("./data/article-2.json")
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            return display(data);
+        })
+        .catch(function (err){
+            console.log(err);
+        });
+}
+function get3() {
+    fetch("./data/article-3.json")
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            return display(data);
+        })
+        .catch(function (err){
+            console.log(err);
+        });
+}
 
+function get4() {
+    fetch("./data/article-4.json")
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            return display(data);
+        })
+        .catch(function (err){
+            console.log(err);
+        });
+}
+
+function get5() {
+    fetch("./data/article-4.json")
+        .then(function (response){
+            return response.json();
+        })
+        .then(function(data){
+            console.log(data);
+            return display(data);
+        })
+        .catch(function (err){
+            console.log(err);
+        });
+}
+
+function display(data){
+    let identifier = 0;
+    const main = document.getElementById("myData");
+    const div = document.createElement("div");
+    const h1 = document.createElement("h1");
+    const p = document.createElement("p");
+    const ul = document.createElement("ul");
+    let img = "";
+    let paras = [];
+    let list = [];
+    let taggedlist = [];
+    for(let i=0; i<data.body.length; i++ ) {
+        if (data.body[i]["type"] === "heading") {
+            let count = data.body[i]["model"]["text"];
+            h1.innerHTML = count;
+            console.log(count)
+        } else if (data.body[i]["type"] === "paragraph") {
+            let count = data.body[i]["model"]["text"];
+            paras.push(count);
+            console.log(count);
+        } else if (data.body[i]["type"] === "image") {
+            div.innerHTML = '<img src = " ' + data.body[i]["model"]["url"] + '"'
+            + ' alt= " ' + data.body[i]["model"]["altText"] + '"'
+            + " height=" + '"' + data.body[i]["model"]["height"] + '"'
+            + ' width="' + data.body[i]["model"]["width"] + '">' ;
+            console.log();
+        } else if (data.body[i]["type"] === "list") {
+            for(let u = 0; u < data.body[i]["model"].items.length; u++){
+                list.push(data.body[i]["model"].items[u]);
+                taggedlist.push('<li>' + list[u] + '</li>');
+            }
+            console.log(taggedlist);
+            ul.innerHTML = '<ul>' + taggedlist + '</ul>';
+            console.log(list);
+        }
+    }
+
+    //console.log("array: " + paras);
+    let cparas = paras.toString();
+    //console.log("collated: " + cparas);
+    p.innerHTML = cparas;
+    main.appendChild(h1);
+    main.appendChild(p);
+    main.appendChild(div);
+    main.appendChild(ul);
+}
+
+
+function clearJSON(){
+    const main = document.getElementById("myData");
+    main.innerHTML="";
 }
